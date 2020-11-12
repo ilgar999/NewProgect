5c1fe7b6e377d011bc245e81824731e42ec65946
IlgarA <ilqar.nureddinoglu@gmail.com>
2020-11-12 10:44:56 -0500

    Working on nice message

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello Git last day");
+        // B21 is excited!!!
 
     }
 }
